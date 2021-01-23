04fc260b444616ded687c56ad0f2d1b1c00cbb24
Hossamghoneim <hossamghoneim11@gmail.com>
2021-01-23 21:33:29 +0200

    ..

--- public/js/pages/employees.js
+++ public/js/pages/employees.js
@@ -231,14 +231,13 @@ jQuery(document).ready(function() {
         }
     })
 
-    $("input[name='contract_start_date']").on('focusout', function () {
+    $("input[name='contract_start_date']").on('change', function () {
         calcEndDate();
     })
 
     $("select[name='contract_period']").on('change', function () {
         calcEndDate();
     })
-
 
 
     function add_years(dt,n)
