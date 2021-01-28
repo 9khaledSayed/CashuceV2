ed8ae478a30712c1873feb5985b22439575a82ed
Hossamghoneim <hossamghoneim11@gmail.com>
2021-01-28 21:03:29 +0200

    export dep & comp

--- app/Http/Controllers/Dashboard/AttendanceController.php
+++ app/Http/Controllers/Dashboard/AttendanceController.php
@@ -314,7 +314,8 @@ class AttendanceController extends Controller
             if(isset($employee)){
 
                 $work_shift = $employee->workShift;
-                $department = $employee->department;
+                $department = $employee->department->name();
+                $company = $employee->company->name();
                 $time_out = $work_shift->type == 'divided' ? $attendance->time_out2 : $attendance->time_out;
                 $shift_start_time = $work_shift->type == 'once' ? $work_shift->check_in_time :  $work_shift->shift_start_time;
                 $shift_work_hours = $work_shift->work_hours;
@@ -342,6 +343,7 @@ class AttendanceController extends Controller
                     'Job Number' => $employee->job_number,
                     'Employee Name' => $employee->{'fname_en'} . ' ' . $employee->{'lname_en'},
                     'Department' => $department,
+                    'Company' => $company,
                     'Date' => $date,
                     'Shift Start Time' => isset($shift_start_time) ? $shift_start_time->format('h:iA') : '',
                     'Time In' => $date . $this->modifyMinutes($attendance->time_in),
