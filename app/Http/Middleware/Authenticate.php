fe5f4185178c769c133ae43119ecef0f67e23168
9khaledSayed <ksayed737@gmail.com>
2021-02-03 20:14:27 +0200

    .

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login.company', $request->route('company'));
+            return route('login.company', 'main');
         }
     }
 }
